bbdf5c9c0270efffd8788aa3d551158001aaa866
Marleysch <marleysch18@gmail.com>
2024-12-10 09:01:02 -0800

    Added all python files

---
+++ Code/Course.py
@@ -0,0 +1,23 @@
+from mongoengine import Document, StringField, IntField, ListField, ReferenceField, NULLIFY
+
+
+class Course(Document):
+    # Fields that represent the columns in the SQL table
+    name = StringField(max_length=30, required=True, primary_key=True)
+    departments_abbr = StringField(max_length=50, required=True)
+    number = IntField(required=True)
+    description = StringField(max_length=100, required=True)
+    units = IntField(required=True)
+    requirements = ListField(ReferenceField('Requirement', reverse_delete_rule=NULLIFY))
+
+    # Unique constraint for the combination of departments_abbr and number
+    meta = {
+        'indexes': [
+            {'fields': ['departments_abbr', 'number'], 'unique': True}
+        ],
+        # Ensure the MongoDB collection is named 'courses'
+        'collection': 'courses'
+    }
+
+    def __str__(self):
+        return f'{self.name} ({self.departments_abbr} {self.number})'
